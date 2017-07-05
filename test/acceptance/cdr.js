56f29abe972261281640536cd630a5bb3dde28c3
Dave Horton <daveh@beachdognet.com>
2017-07-04 22:21:17 -0400

    update to eslint and various bug fixes

--- test/acceptance/cdr.js
+++ test/acceptance/cdr.js
@@ -1,58 +1,55 @@
-var drachtio = require('../..') ;
-var fs = require('fs') ;
-var assert = require('assert');
-var should = require('should');
-var merge = require('merge') ;
-var debug = require('debug')('drachtio-client') ;
-var fixture = require('drachtio-test-fixtures') ;
-var proxy, uas ;
-var cfg = fixture(__dirname,[8040,8041,8042],[6040,6041,6042]) ;
+const drachtio = require('../..') ;
+const should = require('should');
+const merge = require('merge') ;
+const debug = require('debug')('drachtio-client') ;
+const fixture = require('drachtio-test-fixtures') ;
+let proxy;
+const cfg = fixture(__dirname, [8040, 8041, 8042], [6040, 6041, 6042]) ;
 
-function configureUac( app, config ) {
-    app.set('api logger',config.apiLog) ;
-    app.connect(config.connect_opts) ;
-    return app ;
+function configureUac(app, config) {
+  app.set('api logger', config.apiLog) ;
+  app.connect(config.connect_opts) ;
+  return app ;
 }
 
-describe.only('cdr', function() {
-    this.timeout(6000) ;
+describe('cdr', function() {
+  this.timeout(6000) ;
 
-    before(function(done){
-        cfg.startServers(done) ;
+  before((done) => {
+    cfg.startServers(done) ;
+  }) ;
+  after((done) => {
+    cfg.stopServers(done) ;
+  }) ;
+
+  it('should write 1 attempt and 1 stop records when no clients connected', function(done) {
+    const app = drachtio() ;
+    configureUac(app, cfg.client[0]) ;
+    proxy = require('../scripts/cdr/app')(merge({proxyTarget: cfg.sipServer[2], cdrOnly: true}, cfg.client[1])) ;
+    cfg.connectAll([app, proxy], (err) => {
+      if (err) throw err ;
+      debug('connected') ;
+      app.request({
+        uri: cfg.sipServer[1],
+        method: 'INVITE',
+        body: cfg.client[0].sdp,
+        headers: {
+          subject: this.test.fullTitle()
+        }
+      }, (err, req) => {
+        should.not.exist(err) ;
+        req.on('response', (res, ack) => {
+          res.should.have.property('status', 503);
+          ack() ;
+          setTimeout(() => {
+            should.exist(proxy.getAttemptCdr()) ;
+            should.exist(proxy.getStopCdr()) ;
+            proxy.getAttemptCdr().should.have.length(1) ;
+            proxy.getStopCdr().should.have.length(1) ;
+            done() ;
+          }, 100) ;
+        });
+      });
     }) ;
-    after(function(done){
-        cfg.stopServers(done) ;
-    }) ;
- 
-    it('should write 1 attempt and 1 stop records when no clients connected', function(done) {
-        var self = this ;
-        var app = drachtio() ;
-        configureUac( app, cfg.client[0] ) ;
-        proxy = require('../scripts/cdr/app')(merge({proxyTarget: cfg.sipServer[2], cdrOnly: true}, cfg.client[1])) ;
-        cfg.connectAll([app, proxy], function(err){
-            if( err ) throw err ;
-            debug('connected')
-            app.request({
-                uri: cfg.sipServer[1],
-                method: 'INVITE',
-                body: cfg.client[0].sdp,
-                headers: {
-                    subject: self.test.fullTitle()
-                }
-            }, function( err, req ) {
-                should.not.exist(err) ;
-                req.on('response', function(res, ack){
-                    res.should.have.property('status',503); 
-                    ack() ;
-                    setTimeout(function(){
-                        should.exist( proxy.getAttemptCdr() ) ;
-                        should.exist( proxy.getStopCdr() ) ;
-                        proxy.getAttemptCdr().should.have.length(1) ;
-                        proxy.getStopCdr().should.have.length(1) ;
-                        done() ;                        
-                    }, 100) ;
-                }) ;
-            }) ;            
-        }) ;
-    }) ;    
+  }) ;
 }) ;
--- test/acceptance/proxy.js
+++ test/acceptance/proxy.js
@@ -1,247 +1,242 @@
-var drachtio = require('../..') ;
-var fs = require('fs') ;
-var assert = require('assert');
-var should = require('should');
-var merge = require('merge') ;
-var debug = require('debug')('drachtio-client') ;
-var fixture = require('drachtio-test-fixtures') ;
-var cfg = fixture(__dirname,[8060,8061,8062],[6060,6061,6062]) ;
+const drachtio = require('../..') ;
+const should = require('should');
+const merge = require('merge') ;
+const debug = require('debug')('drachtio-client') ;
+const fixture = require('drachtio-test-fixtures') ;
+const cfg = fixture(__dirname, [8060, 8061, 8062], [6060, 6061, 6062]) ;
 
 var proxy, uas ;
 
-function configureUac( app, config ) {
-    app.set('api logger', config.apiLog ) ;
-    app.connect(config.connect_opts) ;
-    return app ;
+function configureUac(app, config) {
+  app.set('api logger', config.apiLog) ;
+  app.connect(config.connect_opts) ;
+  return app ;
 }
 
 describe('proxy', function() {
-    this.timeout(6000) ;
+  this.timeout(6000) ;
 
-    before(function(done){
-        cfg.startServers(done) ;
+  before(function(done) {
+    cfg.startServers(done) ;
+  }) ;
+  after(function(done) {
+    cfg.stopServers(done) ;
+  }) ;
+
+  it('should respond 483 Too Many Hops when Max-Forwards is 0', function(done) {
+    var app = drachtio() ;
+    configureUac(app, cfg.client[0]) ;
+    proxy = require('../scripts/proxy/app')(merge({proxyTarget: cfg.sipServer[2], remainInDialog: true,
+      label: this.test.fullTitle()}, cfg.client[1]));
+    cfg.connectAll([app, proxy], (err) => {
+      if (err) throw err ;
+      app.request({
+        uri: cfg.sipServer[1],
+        method: 'INVITE',
+        headers: {
+          'Max-Forwards': 0,
+          'Subject': this.test.fullTitle()
+        },
+        body: cfg.client[0].sdp
+      }, function(err, req) {
+        should.not.exist(err) ;
+        req.on('response', function(res) {
+          res.should.have.property('status', 483);
+          app.idle.should.be.true ;
+          done() ;
+        }) ;
+      }) ;
     }) ;
-    after(function(done){
-        cfg.stopServers(done) ;
-    }) ;
- 
-    it('should respond 483 Too Many Hops when Max-Forwards is 0', function(done) {
-        var self = this ;
-        var app = drachtio() ;
-        configureUac( app, cfg.client[0] ) ;
-        proxy = require('../scripts/proxy/app')(merge({proxyTarget: cfg.sipServer[2], remainInDialog: true, label: this.test.fullTitle()}, cfg.client[1])); 
-        cfg.connectAll( [app, proxy], function(err){
-            if( err ) throw err ;            
-            app.request({
-                uri: cfg.sipServer[1],
-                method: 'INVITE',
-                headers: {
-                    'Max-Forwards': 0,
-                    'Subject': self.test.fullTitle()
-                },
-                body: cfg.client[0].sdp
-            }, function( err, req ) {
-                should.not.exist(err) ;
-                req.on('response', function(res){
-                    res.should.have.property('status',483); 
-                    app.idle.should.be.true ;
-                    done() ;                        
-                }) ;
-            }) ;
-        }) ;
-    }) ;
+  }) ;
 
-    it('should decrement Max-Forwards when provided', function(done) {
-        var self = this ;
-        var app = drachtio() ;
-        configureUac( app, cfg.client[0] ) ;
-        proxy = require('../scripts/proxy/app')(merge({proxyTarget: cfg.sipServer[2], remainInDialog: false}, cfg.client[1])); 
-        uas = require('../scripts/invite-success-uas-bye/app')(cfg.client[2]) ;
+  it('should decrement Max-Forwards when provided', function(done) {
+    const app = drachtio() ;
+    configureUac(app, cfg.client[0]) ;
+    proxy = require('../scripts/proxy/app')(merge({proxyTarget: cfg.sipServer[2],
+      remainInDialog: false}, cfg.client[1]));
+    uas = require('../scripts/invite-success-uas-bye/app')(cfg.client[2]) ;
 
-        app.bye( function(req,res){
-            res.send(200, function(err, bye){
-                should.not.exist(err) ;
-                app.idle.should.be.true; 
-                done() ;
-            }) ;
-        }) ;
-
-        cfg.connectAll([app, proxy, uas], function(err){
-            if( err )  throw err ;
-
-            debug('sending INVITE');
-            app.request({
-                uri: cfg.sipServer[1],
-                method: 'INVITE',
-                headers: {
-                    'Max-Forwards': 11,
-                    'Subject': self.test.fullTitle()
-                },
-                body: cfg.client[0].sdp
-            }, function( err, req ) {
-                should.not.exist(err) ;
-                req.on('response', function(res, ack){
-                    res.should.have.property('status',200); 
-                    res.get('Max-Forwards').should.eql('10') ;
-                    ack() ;
-                    debug('sent ACK'); 
-                }) ;
-            }) ;            
-        }) ;
-    }) ;   
-    it('should add Record-Route header when remainInDialog is set to true', function(done) {
-        var self = this ;
-        var app = drachtio() ;
-        configureUac( app, cfg.client[0] ) ;
-        proxy = require('../scripts/proxy/app')(merge({proxyTarget: cfg.sipServer[2], remainInDialog: true}, cfg.client[1])); 
-        uas = require('../scripts/invite-success-uas-bye/app')(cfg.client[2]) ;
-
-        app.bye( function(req,res){
-            res.send(200, function(err, bye){
-                should.not.exist(err) ;
-                app.idle.should.be.true; 
-                done() ;
-            }) ;
-        }) ;
-
-        cfg.connectAll([app, proxy, uas], function(err){
-            if( err )  throw err ;
-
-            app.request({
-                uri: cfg.sipServer[1],
-                method: 'INVITE',
-                headers: {
-                    'Subject' : self.test.fullTitle()
-                },
-                body: cfg.client[0].sdp
-            }, function( err, req ) {
-                should.not.exist(err) ;
-                req.on('response', function(res, ack){
-                    res.should.have.property('status',200); 
-                    should.exist( res.get('Record-Route') ) ;
-                    ack() ;
-                }) ;
-            }) ;            
-        }) ;
+    app.bye((req, res) => {
+      res.send(200, (err, bye) => {
+        should.not.exist(err) ;
+        app.idle.should.be.true;
+        done() ;
+      }) ;
     }) ;
 
-    it('should not add Record-Route header when remainInDialog set to false', function(done) {
-        var self = this ;
-        var app = drachtio() ;
-        configureUac( app, cfg.client[0] ) ;
-        proxy = require('../scripts/proxy/app')(merge({proxyTarget: cfg.sipServer[2], remainInDialog: false}, cfg.client[1])); 
-        uas = require('../scripts/invite-success-uas-bye/app')(cfg.client[2]) ;
+    cfg.connectAll([app, proxy, uas], function(err) {
+      if (err) throw err ;
 
-        app.bye( function(req,res){
-            res.send(200, function(err, bye){
-                should.not.exist(err) ;
-                app.idle.should.be.true; 
-                done() ;
-            }) ;
+      debug('sending INVITE');
+      app.request({
+        uri: cfg.sipServer[1],
+        method: 'INVITE',
+        headers: {
+          'Max-Forwards': 11,
+          'Subject': this.test.fullTitle()
+        },
+        body: cfg.client[0].sdp
+      }, (err, req) => {
+        should.not.exist(err) ;
+        req.on('response', (res, ack) => {
+          res.should.have.property('status', 200);
+          res.get('Max-Forwards').should.eql('10') ;
+          ack() ;
+          debug('sent ACK');
         }) ;
+      }) ;
+    }) ;
+  }) ;
+  it('should add Record-Route header when remainInDialog is set to true', function(done) {
+    const app = drachtio() ;
+    configureUac(app, cfg.client[0]) ;
+    proxy = require('../scripts/proxy/app')(merge({proxyTarget: cfg.sipServer[2],
+      remainInDialog: true}, cfg.client[1]));
+    uas = require('../scripts/invite-success-uas-bye/app')(cfg.client[2]) ;
 
-        cfg.connectAll([app, proxy, uas], function(err){
-            if( err )  throw err ;
+    app.bye((req, res) => {
+      res.send(200, (err, bye) => {
+        should.not.exist(err) ;
+        app.idle.should.be.true;
+        done() ;
+      }) ;
+    }) ;
 
-            app.request({
-                uri: cfg.sipServer[1],
-                method: 'INVITE',
-                headers: {
-                    'Subject' : self.test.fullTitle()
-                },
-                body: cfg.client[0].sdp
-            }, function( err, req ) {
+    cfg.connectAll([app, proxy, uas], (err) => {
+      if (err)  throw err ;
+
+      app.request({
+        uri: cfg.sipServer[1],
+        method: 'INVITE',
+        headers: {
+          'Subject' : this.test.fullTitle()
+        },
+        body: cfg.client[0].sdp
+      }, (err, req) => {
+        should.not.exist(err) ;
+        req.on('response', (res, ack) => {
+          res.should.have.property('status', 200);
+          should.exist(res.get('Record-Route')) ;
+          ack() ;
+        }) ;
+      }) ;
+    }) ;
+  }) ;
+
+  it('should not add Record-Route header when remainInDialog set to false', function(done) {
+    const app = drachtio() ;
+    configureUac(app, cfg.client[0]) ;
+    proxy = require('../scripts/proxy/app')(merge({proxyTarget: cfg.sipServer[2],
+      remainInDialog: false}, cfg.client[1]));
+    uas = require('../scripts/invite-success-uas-bye/app')(cfg.client[2]) ;
+
+    app.bye((req, res) => {
+      res.send(200, (err, bye) => {
+        should.not.exist(err) ;
+        app.idle.should.be.true;
+        done() ;
+      }) ;
+    }) ;
+
+    cfg.connectAll([app, proxy, uas], (err) => {
+      if (err)  throw err ;
+
+      app.request({
+        uri: cfg.sipServer[1],
+        method: 'INVITE',
+        headers: {
+          'Subject' : this.test.fullTitle()
+        },
+        body: cfg.client[0].sdp
+      }, (err, req) => {
+        should.not.exist(err) ;
+        req.on('response', (res, ack) => {
+          res.should.have.property('status', 200);
+          should.not.exist(res.get('Record-Route')) ;
+          ack() ;
+        }) ;
+      }) ;
+    }) ;
+  }) ;
+
+  it('should not add Record-Route header by default', function(done) {
+    const app = drachtio() ;
+    configureUac(app, cfg.client[0]) ;
+    proxy = require('../scripts/proxy/app')(merge({proxyTarget: cfg.sipServer[2]}, cfg.client[1]));
+    uas = require('../scripts/invite-success-uas-bye/app')(cfg.client[2]) ;
+
+    app.bye((req, res) => {
+      res.send(200, (err, bye) => {
+        should.not.exist(err) ;
+        app.idle.should.be.true;
+        done() ;
+      }) ;
+    }) ;
+
+    cfg.connectAll([app, proxy, uas], (err) => {
+      if (err)  throw err ;
+      app.request({
+        uri: cfg.sipServer[1],
+        method: 'INVITE',
+        headers: {
+          'Subject': this.test.fullTitle()
+        },
+        body: cfg.client[0].sdp
+      }, (err, req) => {
+        should.not.exist(err) ;
+        req.on('response', function(res, ack) {
+          res.should.have.property('status', 200);
+          should.not.exist(res.get('Record-Route')) ;
+          ack() ;
+        }) ;
+      }) ;
+    }) ;
+  }) ;
+  it.only('should handle handle reliable provisional responses', function(done) {
+    const app = drachtio() ;
+    configureUac(app, cfg.client[0]) ;
+    proxy = require('../scripts/proxy/app')(merge({proxyTarget: cfg.sipServer[2],
+      remainInDialog: true}, cfg.client[1]));
+    uas = require('../scripts/invite-100rel/app')(cfg.client[2]) ;
+
+    cfg.connectAll([app, proxy, uas], (err) => {
+      if (err) throw err ;
+      app.request({
+        uri: cfg.sipServer[1],
+        method: 'INVITE',
+        headers: {
+          'Require': '100rel',
+          'Subject': this.test.fullTitle()
+        },
+        body: cfg.client[0].sdp
+      }, (err, req) => {
+        should.not.exist(err) ;
+        req.on('response', (res, ack) => {
+          if (res.status > 100 && res.status < 200) {
+            const require = res.get('Require') ;
+            require.should.eql('100rel') ;
+            ack() ;
+          }
+          if (res.status >= 200) {
+            res.should.have.property('status', 200);
+            ack() ;
+            //after a short time, send a BYE and validate the response
+            setTimeout(() => {
+              app.request({
+                method: 'BYE',
+                stackDialogId: res.stackDialogId
+              }, (err, bye) => {
                 should.not.exist(err) ;
-                req.on('response', function(res, ack){
-                    res.should.have.property('status',200); 
-                    should.not.exist( res.get('Record-Route') ) ;
-                    ack() ;
+                bye.on('response', (response) => {
+                  response.should.have.property('status', 200);
+                  app.idle.should.be.true ;
+                  done() ;
                 }) ;
-            }) ;            
+              }) ;
+            }, 1) ;
+          }
         }) ;
-    }) ; 
-    
-    it('should not add Record-Route header by default', function(done) {
-        var self = this ;
-        var app = drachtio() ;
-        configureUac( app, cfg.client[0] ) ;
-        proxy = require('../scripts/proxy/app')(merge({proxyTarget: cfg.sipServer[2]}, cfg.client[1])); 
-        uas = require('../scripts/invite-success-uas-bye/app')(cfg.client[2]) ;
-
-        app.bye( function(req,res){
-            res.send(200, function(err, bye){
-                should.not.exist(err) ;
-                app.idle.should.be.true; 
-                done() ;
-            }) ;
-        }) ;
-
-        cfg.connectAll([app, proxy, uas], function(err){
-            if( err )  throw err ;
-            app.request({
-                uri: cfg.sipServer[1],
-                method: 'INVITE',
-                headers: {
-                    'Subject': self.test.fullTitle()
-                },
-                body: cfg.client[0].sdp
-            }, function( err, req ) {
-                should.not.exist(err) ;
-                req.on('response', function(res, ack){
-                    res.should.have.property('status',200); 
-                    should.not.exist( res.get('Record-Route') ) ;
-                    ack() ;
-                }) ;
-            }) ;            
-        }) ;
-    }) ;    
-    it.only('should handle handle reliable provisional responses', function(done) {
-        var self = this ;
-        var app = drachtio() ;
-        configureUac( app, cfg.client[0] ) ;
-        proxy = require('../scripts/proxy/app')(merge({proxyTarget: cfg.sipServer[2], remainInDialog: true}, cfg.client[1])); 
-        uas = require('../scripts/invite-100rel/app')(cfg.client[2]) ;
-
-        cfg.connectAll([app, proxy, uas], function(err){
-            if( err ) throw err ;
-            app.request({
-                uri: cfg.sipServer[1],
-                method: 'INVITE',
-                headers: {
-                    'Require': '100rel',
-                    'Subject': self.test.fullTitle()
-                },
-                body: cfg.client[0].sdp
-            }, function( err, req ) {
-                should.not.exist(err) ;
-                req.on('response', function(res, ack){
-                    if( res.status > 100 && res.status < 200 ) {
-                        var require = res.get('Require') ;
-                        require.should.eql('100rel') ;
-                        ack() ;
-                    }
-                    if( res.status >= 200 ) {
-                        res.should.have.property('status',200); 
-                        ack() ; 
-                       //
-                        //after a short time, send a BYE and validate the response
-                        //
-                        setTimeout( function(){
-                            app.request({
-                                method: 'BYE',
-                                stackDialogId: res.stackDialogId
-                            }, function(err, bye){
-                                should.not.exist(err) ;
-                                bye.on('response', function(response){
-                                    response.should.have.property('status',200);
-                                    app.idle.should.be.true ;
-                                    done() ;
-                                }) ;
-                            }) ;
-                        }, 1) ;
-                    }
-                }) ;
-            }) ;            
-        }) ;
-    }) ;  
+      }) ;
+    }) ;
+  }) ;
 }) ;
